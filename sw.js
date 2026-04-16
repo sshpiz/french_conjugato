ba6146f530e04d025dcf0bfff185e556fff96fa9
Simeon <simeon@Simeons-MacBook-Air.local>
2026-04-16 15:51:45 +0200

    Make French app versions readable and force SW refresh

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // sw.js - scoped stale-while-revalidate for the French app.
 
 const CACHE_PREFIX = 'fr-app-cache-';
-const CACHE_NAME = CACHE_PREFIX + 'v16';
+const CACHE_NAME = CACHE_PREFIX + 'v17';
 const LOG_KEY = '__sw-log';
 const MAX_LOG = 100;
 const SCOPE_PATH = new URL(self.registration.scope).pathname.replace(/\/$/, '');
